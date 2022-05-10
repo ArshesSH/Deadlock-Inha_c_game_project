89e26b3341552b26f22d42356d06ee680ccae5f5
ArshesSH <53480645+ArshesSH@users.noreply.github.com>
2022-05-10 17:47:54 +0900

    Revert to Default

--- Inha_C_Game/main.c
+++ Inha_C_Game/main.c
@@ -5,28 +5,80 @@
 #include "Vec2.h"
 #include "Rect.h"
 #include "Surface.h"
-#include "Game.h"
 
 #include <mmsystem.h>
 #pragma comment(lib,"winmm.lib")
 
-int main(int argc, char* argv[]) {
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
 
-    SetConsoleFontSize(1);
-    SetConsoleWindowSize(1280, 720);
+int main(int argc, char* argv[]) {
+    SetConsoleFontSize( 1 );
+    SetConsoleWindowSize( 1280, 720 );
     HideCursor();
+
+    Vec2 pos = { 100, 100 };
+    int speed = 5;
+    Vec2 up = { 0, -speed };
+    Vec2 down = { 0, speed };
+    Vec2 left = { -(speed), 0 };
+    Vec2 right = { (speed), 0 };
+
+    int key;
+
+    Surface surf;
+    MakeSurface( "src/images/LightTankResize20.bmp", &surf );
+
+    Vec2 lastPos = pos;
 
     //PlaySound(TEXT("src/coin.wav"), NULL, SND_ASYNC);
 
-    InitGame();
-    while (1)
+    // Do Game
+    while ( 1 )
     {
-        UpdateModel();
-        ComposeFrame();
+        // Update Model
+        {
+            if ( _kbhit() )
+            {
+                key = _getch();
+
+                if ( key == UP )
+                {
+                    DeleteRect( SurfaceGetRect( &surf ), pos.x, pos.y );
+                    lastPos = pos;
+                    Vec2AddEqual( &pos, up );
+                }
+                if ( key == DOWN )
+                {
+                    DeleteRect( SurfaceGetRect( &surf ), pos.x, pos.y );
+                    lastPos = pos;
+                    Vec2AddEqual( &pos, down );
+                }
+                if ( key == LEFT )
+                {
+                    DeleteRect( SurfaceGetRect( &surf ), pos.x, pos.y );
+                    lastPos = pos;
+                    Vec2AddEqual( &pos, left );
+                }
+                if ( key == RIGHT )
+                {
+                    DeleteRect( SurfaceGetRect( &surf ), pos.x, pos.y );
+                    lastPos = pos;
+                    Vec2AddEqual( &pos, right );
+                }
+            }
+        }
+
+        // Compose Frame
+        {
+            DrawSpriteNonChroma( pos.x, pos.y, &surf );
+            Sleep( 2 );
+        }
     }
-    EndGame();
-    
-    _getch();
+
+    DeleteSurface( &surf );
 
     return 0;
 }
